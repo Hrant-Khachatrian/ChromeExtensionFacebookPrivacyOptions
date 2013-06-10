7f389977811fc11d36a653fe3833978dc1597909
Hrant <hrant@egern.net>
2013-06-11 02:02:43 +0400

    version 0.1

---
+++ facebookPrivacy.js
@@ -0,0 +1,14 @@
+﻿// Inspired by an image from the web
+(function () {
+	var refresh = function () {
+		var t = document.querySelectorAll('.fbPrivacyAudienceSelectorOption .itemLabel:not(.CIA)');
+		for(var i in t){
+			if(t[i].innerHTML != 'Public'){
+				t[i].innerHTML += ' and the CIA';
+			}
+			t[i].className += ' CIA';
+		}
+		setTimeout(refresh, 1000);
+	};
+	setTimeout(refresh, 1000);
+})();
